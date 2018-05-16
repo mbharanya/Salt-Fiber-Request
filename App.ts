9d1f8f02d3d3a3cc85009395666e7fb1d13aa674
Milan Bharanya <milan.bharanya@tundra.com>
2018-05-16 08:40:49 +0200

    Initial commit

---
+++ App.ts
@@ -0,0 +1,17 @@
+import * as request from 'request'
+
+const url = "https://fixnet-api.salt.ch/api/AddressScoring"
+
+const input = process.argv[2]
+const parts: string[] = input.replace(",", "").replace(/\s\s+/g, ' ').split(" ")
+
+const rq = {
+    street: parts[0],
+    house: parts[1],
+    zip: parts[2],
+    city: parts[3]
+}
+
+request.post({ url: url, form: rq }, function (err, httpResponse, body) {
+    console.log(JSON.parse(body))
+})
